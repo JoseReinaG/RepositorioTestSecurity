1b606c59e425a5f81ff823abba456ae67ad0a08c
JReinaG <rgarcia.pepe@gmail.com>
2020-11-05 12:47:38 +0100

    TestCase

--- testSecurityException-master/src/test/java/com/cjgmj/testSecurityException/TestSecurityExceptionApplicationTests.java
+++ testSecurityException-master/src/test/java/com/cjgmj/testSecurityException/TestSecurityExceptionApplicationTests.java
@@ -1,13 +1,40 @@
 package com.cjgmj.testSecurityException;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
-@SpringBootTest
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.cjgmj.testSecurityException.entity.UserEntity;
+
+import com.cjgmj.testSecurityException.service.IUserService;
+import com.cjgmj.testSecurityException.TestSecurityExceptionApplication;
+
+
+@SpringBootTest(classes = TestSecurityExceptionApplication.class)
 class TestSecurityExceptionApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private IUserService userService;
 
+	@Test
+	public void TestListarUsuarios() {
+		
+		List<UserEntity> listaUsuarios = new ArrayList();
+				
+		listaUsuarios=userService.findAll();
+		assertNull(listaUsuarios);
+		
+		
+	}
+	
+
+	
 }
